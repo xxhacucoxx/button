0bf4a4db1ca7318c6bebf1bd3d6029fc47b0c649
xxhacucoxx <xxhacucoxx.celestial@gmail.com>
2023-09-28 04:21:29 +0800

    first-commit

---
+++ index.js
@@ -0,0 +1,182 @@
+$('.e1').click(function() {
+    $('#aa').get(0).play();
+    // alert("eee")
+})
+
+$('.p1').click(function() {
+    $('#p1').get(0).play();
+    
+})
+
+$('.p2').click(function() {
+    $('#p2').get(0).play();
+    
+})
+$('.p3').click(function() {
+    $('#p3').get(0).play();
+    
+})
+$('.p4').click(function() {
+    $('#p4').get(0).play();
+    
+})
+$('.p5').click(function() {
+    $('#p5').get(0).play();
+    
+})
+$('.p6').click(function() {
+    $('#p6').get(0).play();
+    
+})
+$('.p7').click(function() {
+    $('#p7').get(0).play();
+    
+})
+$('.p8').click(function() {
+    $('#p8').get(0).play();
+    
+})
+$('.p9').click(function() {
+    $('#p9').get(0).play();
+  
+})
+$('.p10').click(function() {
+    $('#p10').get(0).play();
+  
+})
+$('.p11').click(function() {
+    $('#p11').get(0).play();
+  
+})
+$('.p12').click(function() {
+    $('#p12').get(0).play();
+  
+})
+$('.p13').click(function() {
+    $('#p13').get(0).play();
+  
+})
+$('.p14').click(function() {
+    $('#p14').get(0).play();
+  
+})
+$('.p15').click(function() {
+    $('#p15').get(0).play();
+  
+})
+$('.p16').click(function() {
+    $('#p16').get(0).play();
+  
+})
+$('.p17').click(function() {
+    $('#p17').get(0).play();
+  
+})
+$('.p18').click(function() {
+    $('#p18').get(0).play();
+  
+})
+$('.p19').click(function() {
+    $('#p19').get(0).play();
+  
+})
+$('.p20').click(function() {
+    $('#p20').get(0).play();
+  
+})
+$('.p20').click(function() {
+    $('#p20').get(0).play();
+  
+})
+$('.p21').click(function() {
+    $('#p21').get(0).play();
+  
+})
+$('.p22').click(function() {
+    $('#p22').get(0).play();
+  
+})
+$('.p23').click(function() {
+    $('#p23').get(0).play();
+  
+})
+$('.p24').click(function() {
+    $('#p24').get(0).play();
+  
+})
+$('.p25').click(function() {
+    $('#p25').get(0).play();
+  
+})
+$('.p26').click(function() {
+    $('#p26').get(0).play();
+  
+})
+$('.p27').click(function() {
+    $('#p27').get(0).play();
+  
+})
+$('.p28').click(function() {
+    $('#p28').get(0).play();
+  
+})
+$('.p29').click(function() {
+    $('#p29').get(0).play();
+  
+})
+$('.p30').click(function() {
+    $('#p30').get(0).play();
+  
+})
+$('.p31').click(function() {
+    $('#p31').get(0).play();
+  
+})
+$('.p32').click(function() {
+    $('#p32').get(0).play();
+  
+})
+$('.p33').click(function() {
+    $('#p33').get(0).play();
+  
+})
+$('.p34').click(function() {
+    $('#p34').get(0).play();
+  
+})
+$('.p35').click(function() {
+    $('#p35').get(0).play();
+  
+})
+$('.p36').click(function() {
+    $('#p36').get(0).play();
+  
+})
+$('.p37').click(function() {
+    $('#p37').get(0).play();
+  
+})
+$('.p38').click(function() {
+    $('#p38').get(0).play();
+  
+})
+$('.p39').click(function() {
+    $('#p39').get(0).play();
+  
+})
+$('.p40').click(function() {
+    $('#p40').get(0).play();
+  
+})
+$('.p41').click(function() {
+    $('#p41').get(0).play();
+  
+})
+$('.p42').click(function() {
+    $('#p42').get(0).play();
+  
+})
+$('.p43').click(function() {
+    $('#p43').get(0).play();
+  
+})
